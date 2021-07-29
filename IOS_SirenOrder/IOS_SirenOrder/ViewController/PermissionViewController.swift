40974940bb63d8a4606ef6294af5215b7298a696
ReplaceMyBrain <mener_marine@naver.com>
2021-07-29 18:09:24 +0900

    2021.07.29 18.09 김도우
    cover, permission

---
+++ IOS_SirenOrder/IOS_SirenOrder/ViewController/PermissionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PermissionViewController.swift
+//  IOS_SirenOrder
+//
+//  Created by RayAri on 2021/07/29.
+//
+
+import UIKit
+
+class PermissionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
