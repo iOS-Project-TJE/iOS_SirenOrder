11a1bba353225a73b41c0c80b22d7e17e81c74a2
ReplaceMyBrain <mener_marine@naver.com>
2021-07-29 21:58:31 +0900

    2021.07.29 21:57 김도우
    cover / permission / explain 뷰 완성

--- IOS_SirenOrder/IOS_SirenOrder/ViewController/CoverViewController.swift
+++ IOS_SirenOrder/IOS_SirenOrder/ViewController/CoverViewController.swift
@@ -9,12 +9,43 @@ import UIKit
 
 class CoverViewController: UIViewController {
 
+    let interval = 1.0
+    let timeSelector : Selector = #selector(CoverViewController.updateTime)
+    var change = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0, green: 0.5628422499, blue: 0.3188166618, alpha: 1)
+        
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+        
     }
     
+    @objc func updateTime() {
+        let formatter = DateFormatter()
+        
+        formatter.locale = Locale(identifier: "ko")
+        formatter.dateFormat = "ss"
+        
+        if change {
+            moveNext()
+        }else{
+            change = !change
+        }
+    }
+    
+    
+    func moveNext() {
+        guard let uvc = self.storyboard?.instantiateViewController(withIdentifier: "PermissionVC") else{
+            return
+        }
+
+        uvc.modalTransitionStyle = UIModalTransitionStyle.coverVertical
+
+        self.present(uvc, animated: true)
+    }
 
     /*
     // MARK: - Navigation
