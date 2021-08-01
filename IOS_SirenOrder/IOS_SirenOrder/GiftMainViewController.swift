dc20edb00febe0f42ea47b0d2714b9560fd76442
Yang seolin <81757940+bisor0627@users.noreply.github.com>
2021-08-02 03:48:00 +0900

    0802 03:47 양서린
    tab bar
    1.간격 조절하기
    2.색상 변경하기

--- IOS_SirenOrder/IOS_SirenOrder/GiftMainViewController.swift
+++ IOS_SirenOrder/IOS_SirenOrder/GiftMainViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 import Tabman
 import Pageboy
 
-class GiftMainViewController: UIViewController {
+class GiftMainViewController: TabmanViewController {
+    
+    var viewControllers: Array<UIViewController> = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let vc2 = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "giftItems") as! GiftCardItemViewController
+        let vc3 = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "giftLists") as! GiftCardListViewController
 
+       viewControllers.append(vc2)
+       viewControllers.append(vc3)
+        
         self.dataSource = self
 
         // Create bar
@@ -24,14 +32,61 @@ class GiftMainViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
+    /* 필수 변경사항
+     tab bar
+     1.간격 조절하기
+     2.색상 변경하기
     */
+    
+    func settingTabBar (ctBar : TMBar.ButtonBar) {
+        ctBar.layout.transitionStyle = .snap
+        // 왼쪽 여백주기
+        ctBar.layout.contentInset = UIEdgeInsets(top: 0.0, left: 50.0, bottom: 0.0, right: 50.0)
+        
+        // 간격
+        ctBar.layout.interButtonSpacing = 100
+            
+        ctBar.backgroundView.style = .blur(style: .light)
+        
+        // 선택 / 안선택 색 + font size
+        ctBar.buttons.customize { (button) in
+            button.tintColor = UIColor.black
+            button.selectedTintColor = .black
+            button.font = UIFont.systemFont(ofSize: 16)
+            button.selectedFont = UIFont.systemFont(ofSize: 16, weight: .medium)
+        }
+        
+        // 인디케이터
+        ctBar.indicator.weight = .custom(value: 2)
+        ctBar.indicator.tintColor = UIColor.green
+    }
 
+}
+extension GiftMainViewController: PageboyViewControllerDataSource, TMBarDataSource {
+    
+    func barItem(for bar: TMBar, at index: Int) -> TMBarItemable {
+   
+        // MARK: -Tab 안 글씨들
+        switch index {
+        case 1:
+            return TMBarItem(title: "선물내역")
+        default:
+            return TMBarItem(title: "선물하기")
+        }
+
+    }
+    
+    func numberOfViewControllers(in pageboyViewController: PageboyViewController) -> Int {
+        //위에서 선언한 vc array의 count를 반환합니다.
+        return viewControllers.count
+    }
+
+    func viewController(for pageboyViewController: PageboyViewController,
+                        at index: PageboyViewController.PageIndex) -> UIViewController? {
+        return viewControllers[index]
+    }
+
+    func defaultPage(for pageboyViewController: PageboyViewController) -> PageboyViewController.Page? {
+        return nil
+    }
 }
