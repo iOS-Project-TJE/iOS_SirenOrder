029e06e5c84d82c82de5268a38bd0973ad4ba5d5
Yang seolin <81757940+bisor0627@users.noreply.github.com>
2021-08-02 11:20:45 +0900

    0802 11:20 양서린
    Card Model 추가

--- IOS_SirenOrder/IOS_SirenOrder/GiftCardItemViewController.swift
+++ IOS_SirenOrder/IOS_SirenOrder/GiftCardItemViewController.swift
@@ -8,22 +8,30 @@
 import UIKit
 
 class GiftCardItemViewController: UIViewController {
-
+    var allCardImgList:[String] = []
+    var mainCardImgList:[String] = []
+    @IBOutlet weak var cardImgChangeControler: UIPageControl!
+    @IBOutlet weak var giftCardImgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        inputImages()
+        
+        cardImgChangeControler.numberOfPages = mainCardImgList.count
+        cardImgChangeControler.currentPage = 0
+        cardImgChangeControler.pageIndicatorTintColor = UIColor.gray
+        cardImgChangeControler.currentPageIndicatorTintColor = UIColor.darkGray
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func inputImages() {
+        
     }
-    */
+
+    @IBAction func cardImgPageChange(_ sender: UIPageControl) {
+        giftCardImgView.image = UIImage(named: mainCardImgList[cardImgChangeControler.currentPage])
+    }
+
 
 }
